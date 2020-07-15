b55bb0145560a22a6ffedf3388ee6738561ed056
Voldy21 <jonahlvngstn@gmail.com>
2020-07-14 23:33:32 -0400

    changed login

--- Milestone2/Cheese/inc/login.php
+++ Milestone2/Cheese/inc/login.php
@@ -20,46 +20,35 @@ if(isset($_POST['login'])){
             exit();
         }
         //PREPARE SQL STATEMENT
-        $sql = "SELECT * FROM users WHERE username=? OR email=?";
-        $stmt = mysqli_stmt_init($con);
+        $sql = "SELECT * FROM users WHERE username='$username' OR email='$email'";
+        $result = mysqli_query($con, $sql);
         
-        //CHECK IF CONNECTION IS MADE
-        if(!mysqli_stmt_prepare($stmt, $sql)){
-            $_SESSION['login_error'] = "SQL error";
-            header('location: ../login.php');
-            exit();
-        }
-        else{ //QUERY THE DATABASE -- Check if user exists
-            mysqli_stmt_bind_param($stmt, "ss", $username, $username);
-            mysqli_stmt_execute($stmt);
-            $result = mysqli_stmt_get_result($stmt);
-                if($row = mysqli_fetch_assoc($result)){ 
-                    $pwdCheck = password_verify($password, $row['password']);
-                    if($pwdCheck == false){
-                        $_SESSION['login_error'] = "password is not correct";
-                        header("location: ../login.php");
-                        exit();
-                    }
-                    else if($pwdCheck == true){
-                        $_SESSION['user_id'] = $row['user_id'];
-                        $_SESSION['success'] = "You are logged in!";
-                        header('location: ../index.php');
-                        exit();
-                    }
-                    else{
-                        $_SESSION['login_error'] = "unknown error occurred";
-                        header("location: ../login.php");
-                        exit();
-                        
-                    }
-                }
-                else{
-                    $_SESSION['login_error'] = "User not found";
+            if($row = mysqli_fetch_assoc($result)){ 
+                $pwdCheck = password_verify($password, $row['password']);
+                if($pwdCheck == false){
+                    $_SESSION['login_error'] = "password is not correct";
                     header("location: ../login.php");
                     exit();
                 }
-                
+                else if($pwdCheck == true){
+                    $_SESSION['user_id'] = $row['user_id'];
+                    $_SESSION['success'] = "You are logged in!";
+                    header('location: ../index.php');
+                    exit();
+                }
+                else{
+                    $_SESSION['login_error'] = "unknown error occurred";
+                    header("location: ../login.php");
+                    exit();
+
+                }
             }
+            else{
+                $_SESSION['login_error'] = "User not found";
+                header("location: ../login.php");
+                exit();
+            }
+
         }
     
 }
